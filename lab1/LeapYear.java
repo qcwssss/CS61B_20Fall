86dd11d6d7ae184149c2ef99efec55b1c87a6425
Chen Qiu <qiu.chen1@northeastern.edu>
2021-05-11 20:11:12 -0700

    LeapYear complete

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,11 +1,23 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author Chen Qiu
+    # my first java file through Git
  */
 public class LeapYear {
 
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
+    public static boolean isLeapYear(int year){
+        if (year % 400 == 0){
+            return true;
+        }
+        else if (year % 4 == 0 && year % 100 != 0){
+            return true;
+        } 
+        else{
+            return false;
+        }
+    }
     private static void checkLeapYear(int year) {
         if (isLeapYear(year)) {
             System.out.printf("%d is a leap year.\n", year);
