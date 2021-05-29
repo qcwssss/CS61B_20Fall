6d96222c72805272db2b0579792fd9cf85636e43
Chen Qiu <qiu.chen1@northeastern.edu>
2021-05-28 21:21:53 -0700

    Update addLast(), size() methods.

--- lab2/SLList/SLList.java
+++ lab2/SLList/SLList.java
@@ -18,11 +18,43 @@ public class SLList {
         return first.item;
     }
 
+    /** Add an item to the last of the list */
+    public void addLast(int x) {
+        IntNode p = first;
+        while (p.next != null) {
+            p = p.next;
+        }
+        p.next = new IntNode(x, null) ;
+    }
+/** Seems void type can't use Recursive
+ *
+    public void addLastRecur(int x) {
+        if (first.next = null) {
+            first.next = new IntNode(x, null);
+        }
+        return addLastRecur
+    }
+ *
+ */
+
+    /** Returns the size of the list starting at IntNode p. */
+    public static int size(IntNode p) {
+        if (p.next == null) {
+            return 1;
+        }     
+        return 1 + size(p.next);
+    }
+
+    public int size() {
+        return size(first);
+    }
+
     public static void main(String[] args) {
         /** Create a list of integers */
         SLList L = new SLList(15);
         L.addFirst(10);
         L.addFirst(5);
+        L.addLast(20);
 
        System.out.println(L.getFirst());
 
