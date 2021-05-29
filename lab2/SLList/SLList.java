51779599b3a3bd4de2a68bd5661398d0e1fbcaea
Chen Qiu <qiu.chen1@northeastern.edu>
2021-05-29 11:36:22 -0700

    Update size, empty list

--- lab2/SLList/SLList.java
+++ lab2/SLList/SLList.java
@@ -1,17 +1,36 @@
 /** An SLList is a list of integers, which hide us 
  * from the naked truth*/
-
 public class SLList {
-    public IntNode first;
+    private IntNode first;
+    private int size;
+
+    /** nested class */
+    private static class IntNode {
+        public int item;
+        public IntNode next;
+
+        public IntNode(int i, IntNode n) {
+            item = i;
+            next = n;
+        }
+    }
 
     /** Constructor */
     public SLList(int x) {
         first = new IntNode(x, null);
+        size ++;
+    }
+
+    /** Build an empty list. */
+    public SLList() {
+        first = null;
+        size = 0;
     }
 
     /** Adds an item to the front of the list. */
     public void addFirst(int x) {
         first = new IntNode(x, first);
+        size ++;
     }
     /** Retrieves the front item from the list. */
     public int getFirst() {
@@ -21,32 +40,21 @@ public class SLList {
     /** Add an item to the last of the list */
     public void addLast(int x) {
         IntNode p = first;
+        if (first == null) {
+            first = new IntNode(x, null);
+            size ++;
+            return;
+        }
+
         while (p.next != null) {
             p = p.next;
         }
         p.next = new IntNode(x, null) ;
-    }
-/** Seems void type can't use Recursive
- *
-    public void addLastRecur(int x) {
-        if (first.next = null) {
-            first.next = new IntNode(x, null);
-        }
-        return addLastRecur
-    }
- *
- */
-
-    /** Returns the size of the list starting at IntNode p. */
-    public static int size(IntNode p) {
-        if (p.next == null) {
-            return 1;
-        }     
-        return 1 + size(p.next);
+        size ++;
     }
 
     public int size() {
-        return size(first);
+        return size;
     }
 
     public static void main(String[] args) {
@@ -55,8 +63,12 @@ public class SLList {
         L.addFirst(10);
         L.addFirst(5);
         L.addLast(20);
+        // an empty list
+        SLList empty = new SLList();
+        empty.addLast(9);
 
-       System.out.println(L.getFirst());
+        System.out.println(empty.size());
+
 
     }
 }
