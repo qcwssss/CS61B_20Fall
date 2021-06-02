a530928edaf1f2918d257aa8149d860fb49677da
Chen Qiu <qiu.chen1@northeastern.edu>
2021-06-02 15:11:34 -0700

    update launcher

---
+++ lab2/SLList/SLListLauncher.java
@@ -0,0 +1,8 @@
+public class SLListLauncher{
+    public static void main(String[] args) {
+        SLList<String> s1 = new SLList<String> ("bone");
+        s1.addFirst("flesh");
+
+        //System.out.println("&s", s1.getFirst);
+    }
+}
