8700f1e724e463dfefc7076834870145b931a4c8
Chen Qiu <qiu.chen1@northeastern.edu>
2021-06-06 10:31:34 -0700

    Alist ver1.5, need to add reSize()

--- 2.5Alist/DIY/AList.java
+++ 2.5Alist/DIY/AList.java
@@ -13,34 +13,46 @@
 */
 
 public class AList {
-    private int item[];
+    private int items[];
     private int size;
 
     /** Creates an empty list. */
     public AList() {
+        items = new int[100];
+        size = 0;
     }
 
     /** Inserts X into the back of the list. */
     public void addLast(int x) {
+        if (size == items.length) {
+            int[] a = new int[size + 1];
+            System.arraycopy(items, 0, a, 0, size);
+            items = a;
+            return;
+        }
+        items[size] = x;
     }
 
     /** Returns the item from the back of the list. */
     public int getLast() {
-        return 0;        
+        return items[size - 1];        
     }
     /** Gets the ith item in the list (0 is the front). */
     public int get(int i) {
-        return 0;        
+        return items[i];        
     }
 
     /** Returns the number of items in the list. */
     public int size() {
-        return 0;        
+        return size;        
     }
 
-    /** Deletes item from back of the list and
+    /** Deletes items from back of the list and
       * returns deleted item. */
     public int removeLast() {
-        return 0;
+        int last = this.getLast();
+        // items[size] = 0; fine, but unnecessary
+        size--;
+        return last;
     }
 } 
