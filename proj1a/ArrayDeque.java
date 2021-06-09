d7a6e261b9169c1d35a877253784e725169a3de7
Chen Qiu <qiu.chen1@northeastern.edu>
2021-06-08 17:24:14 -0700

    methods test completed.

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -3,8 +3,11 @@ public class ArrayDeque<T> {
     private int nextLast;
     private int nextFirst;
     private int size;
-    // Add circular method
-    protected static int capacity = 10;
+
+    // initialize max capacity
+    private int capacity = 8;
+
+
     /** Constructor to create an empty ArrayDeque. */
     public ArrayDeque() {
         items = (T[]) new Object[capacity];
@@ -15,6 +18,8 @@ public class ArrayDeque<T> {
     }
 
     /**
+     * Getters helper methods.
+     *
      * Return the value of nextFirst.
      * @return nextFirst.
      */
@@ -23,31 +28,91 @@ public class ArrayDeque<T> {
     }
 
     /**
+     * Return the integer nextLast.
+     * @return nextLast.
+     */
+    public int getNextLast() {
+        return this.nextLast;
+    }
+
+    /**
+     * Get the capacity of this deque.
+     * @return capacity.
+     */
+    public int getCapacity() {
+        return this.capacity;
+    }
+
+    /**
      * Add the item to the nextFirst position.
-     * @param item
+     * @param item the item will be added.
      */
     public void addFirst(T item) {
-        if (nextFirst >= 0 && nextFirst < capacity) {
+        if (!isFull()) {
+            if (nextFirst < 0) {
+                nextFirst = circulateNext(nextFirst);
+            }
             items[nextFirst] = item;
-            size++;
             nextFirst--;
+            size++;
+
+            return;
         }
+        System.out.println("--Error, deque already full.--");
+        System.out.println("--Can't addFirst item: " + item + " --");
+
     }
 
     /**
      * Add item to the nextLast position.
-     * @param item
+     * @param item the item will be added.
      */
     public void addLast(T item) {
-        if (nextLast < capacity && nextLast > 1) {
+        if (!isFull()) {
+            if (nextLast > capacity -1) {
+                // circulate nextLast to 0
+                nextLast = circulateNext(nextLast);
+            }
             items[nextLast]  = item;
             nextLast++;
             size++;
+
+            return;
         }
+        System.out.println("--Error, deque already full.--");
+        System.out.println("--Can't addLast item: " + item + " --");
+
     }
 
     /**
-     * Return true if the deque is full,
+     * Circulate nextLast, nextFirst to make the
+     * deque functions as a circle array.
+     */
+    private int circulateNext(int nextIndex){
+        if (nextIndex > capacity -1) {
+            nextIndex = 0;
+        }
+        else if (nextIndex < 0) {
+            nextIndex = capacity - 1;
+        } else {
+        String err =
+                "---Error, next index is not exhausted yet--- ";
+        System.out.println(err);
+        }
+        return nextIndex;
+    }
+
+    /**
+     * check whether the index is in the range of
+     * valid value, instead of null.
+     */
+    private void checkIndex() {
+
+    }
+
+
+    /**
+     * Return true if the deque is empty.
      * else, return false.
      * @return boolean value.
      */
@@ -56,6 +121,17 @@ public class ArrayDeque<T> {
             return false;
         }
         return true;
+    }
+
+    /**
+     * Return true if deque is full, otherwise, false.
+     * @return a boolean value.
+     */
+    public boolean isFull() {
+        if (size >= capacity) {
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -86,6 +162,18 @@ public class ArrayDeque<T> {
         if (isEmpty()) {
             return null;
         }
+        if (nextFirst > capacity -1) {
+            nextFirst++; // reset next
+            nextFirst = circulateNext(nextFirst);
+        }
+        else if (nextFirst == capacity -1) {
+            nextFirst = -1;
+            T val = items[nextFirst + 1];
+            items[nextFirst + 1] = null;
+            nextFirst++;
+            size--;
+            return val;
+        }
         T val = items[nextFirst + 1];
         items[nextFirst + 1] = null;
         nextFirst++;
@@ -94,20 +182,45 @@ public class ArrayDeque<T> {
     }
 
     /**
-     * Removes and return the last item.
-     *
+     * Removes and return the last item.     *
      * @return last item of the circular array.
      */
     public T removeLast() {
         if (isEmpty()) {
+            System.out.println("*** Cannot remove an empty deque ***");
             return null;
+        }
+        if (nextLast < 0) {
+            nextLast = circulateNext(nextLast);
+        }
+        else if (nextLast == 0) {
+            nextLast = capacity;
+            T val = items[nextLast - 1];
+            items[nextLast - 1] = null;
+            nextLast--;
+            size--;
+            return val;
         }
         T val = items[nextLast - 1];
         items[nextLast - 1] = null;
         nextLast--;
         size--;
         return val;
-
     }
+
+    /**
+     * Get the value of deque in given index.
+     * @param index the index of array deque.
+     * @return item.
+     */
+    public T get(int index) {
+        if (isEmpty()) {
+            return null;
+        }
+        T val = items[index];
+        return val;
+    }
+
+
 
 }
--- proj1a/ArrayDequeTest.java
+++ proj1a/ArrayDequeTest.java
@@ -2,6 +2,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * Test for ArrayDeque class.
  *
@@ -19,19 +21,43 @@ public class ArrayDequeTest {
 	}
 	@Test
 	public void testIsEmpty() {
-		boolean expected = true;
 		boolean actual = deque.isEmpty();
 		// test size
-		Assert.assertEquals(0, deque.size());
+		assertEquals(0, deque.size());
 
-        Assert.assertEquals(expected, actual);
+        assertEquals(true, actual);
 	}
 
 	@Test
-	public void testGetNextFirst() {
-		Assert.assertEquals(5, deque.getNextFirst());
+	public void testIsFull() {
+		assertEquals(false, deque.isFull());
+
+		// fill array deque1
+		deque1.addFirst("fill");
+		deque1.addFirst("to");
+		deque1.addFirst("going");
+		deque1.addFirst("am");
+		deque1.addFirst("I");
+
+		deque1.addLast("this");
+		deque1.addLast("Array");
+		deque1.addLast("Deque1");
+
+		deque1.printDeque();
+		// test capacity and size
+		assertEquals(8, deque1.getCapacity() );
+		assertEquals(8,deque1.size());
+
+		assertEquals(true, deque1.isFull());
 	}
 
+	@Test
+	public void testGetters() {
+		assertEquals(4, deque.getNextFirst());
+		assertEquals(5, deque.getNextLast());
+		assertEquals(8, deque.getCapacity());
+
+	}
 
 	@Test
 	public void testAddFirst() {
@@ -39,9 +65,17 @@ public class ArrayDequeTest {
 		deque.addFirst(3);
 		deque.addFirst(2);
 		deque.addFirst(1);
-		Assert.assertEquals(2, deque.getNextFirst());
 		// test size
-		Assert.assertEquals(3, deque.size());
+		assertEquals(3, deque.size());
+
+		deque.addFirst(4);
+		deque.addFirst(5);
+		deque.addFirst(6);
+		assertEquals(6, deque.getNextFirst());
+
+		deque.addFirst(7);
+		deque.addFirst(8);
+		deque.addFirst(1001);// add after full
 
 		deque.printDeque();
 	}
@@ -54,30 +88,99 @@ public class ArrayDequeTest {
 		deque.printDeque();
 
 		int actual = deque.removeFirst();
-		Assert.assertEquals(4, actual);
+		assertEquals(4, actual);
 
 		int actual1 = deque.removeFirst();
-		Assert.assertEquals(5, actual1);
+		assertEquals(5, actual1);
 
 		int actual2 = deque.removeFirst();
-		Assert.assertEquals(6, actual2);
+		assertEquals(6, actual2);
+
+		// test cycle
+		deque.addFirst(6);
+		deque.addFirst(5);
+		deque.addFirst(4);
+		deque.addFirst(3);
+		deque.addFirst(2);
+		deque.addFirst(1);
+		int actual3 = deque.removeFirst();
+		assertEquals(1, actual3);
+		int actual4 = deque.removeFirst();
+		assertEquals(2, actual4);
 	}
 
 	@Test
 	public void testRemoveLast() {
-		deque1.addFirst("c");
-		deque1.addFirst("b");
-		deque1.addFirst("a");
-		deque1.printDeque();
+		deque1.addLast("a");
+		deque1.addLast("b");
+		deque1.addLast("c");
 		// test
 		String actual = deque1.removeLast();
-		Assert.assertEquals("c", actual);
+		assertEquals("c", actual);
 
 		String actual1 = deque1.removeLast();
-		Assert.assertEquals("b", actual1);
+		assertEquals("b", actual1);
 
 		String actual2 = deque1.removeLast();
-		Assert.assertEquals("a", actual2);
+		assertEquals("a", actual2);
+
+		// test circulate
+		deque1.addLast("a");
+		deque1.addLast("b");
+		deque1.addLast("c");
+		deque1.addLast("head");
+		assertEquals(1,deque1.getNextLast());
+		assertEquals("head", deque1.removeLast());
+		assertEquals("c", deque1.removeLast());
+
+		deque1.printDeque();
+	}
+	/**
+	 * This test passed, set circulateNext private.
+	@Test
+	public void testCirculateNext() {
+		int input = deque1.getCapacity() - 1;
+		int actual = deque1.circulateNext(input);
+
+		int input1 = 0;
+		int actual1 = deque.circulateNext(input1);
+
+		int input2 = 3;
+		int actual2 = deque.circulateNext(input2);
+
+		assertEquals(0, actual);
+		assertEquals(7, actual1);
+		assertEquals(3, actual2);
+	}
+	 */
+
+
+	@Test
+	public void testGet() {
+		deque1.addLast("head");
+		deque1.addLast(" * ");
+		deque1.addLast(" ** ");
+		deque1.addLast("tail");
+		deque1.printDeque();
+
+		String expected = "head";
+		String actual = deque1.get(deque1.getNextLast()-1);
+
+		assertEquals(expected, deque1.get(5));
+		assertEquals(" * ", deque1.get(6));
+		assertEquals(" ** ", deque1.get(7));
+		assertEquals("tail", actual);
+
+		// test the index of nextLast
+		assertEquals(1, deque1.getNextLast());
+		// add item out of bound
+		deque1.addLast("Whoops!");
+		deque1.addLast("more");
+		deque1.addLast("and");
+		deque1.addLast("more!");
+
+		deque1.addLast("It should explode now!");
+
 
 	}
 
