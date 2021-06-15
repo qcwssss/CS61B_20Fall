c6a9445697b650e0cc27bf3ff58a752a783c5574
Chen Qiu <qiu.chen1@northeastern.edu>
2021-06-15 11:10:50 -0700

    update offByOne

--- proj1b/OffByOne.java
+++ proj1b/OffByOne.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class OffByOne {
+public class OffByOne implements CharacterComparator{
+	@Override
+	public boolean equalChars(char x, char y) {
+		int diff = x - y;
+		if (Math.abs(diff) == 1){
+			return true;
+		}
+		return false;
+	}
 }
--- proj1b/PalindromeFinder.java
+++ proj1b/PalindromeFinder.java
@@ -1,17 +1,32 @@
 
 /** This class outputs all palindromes in the words file in the current directory. */
-/**public class PalindromeFinder {
+public class PalindromeFinder {
     public static void main(String[] args) {
         int minLength = 4;
         In in = new In("../library-fa20/data/words.txt");
         Palindrome palindrome = new Palindrome();
+        OffByOne offByOne = new OffByOne();
 
+        /**
         while (!in.isEmpty()) {
             String word = in.readString();
             if (word.length() >= minLength && palindrome.isPalindrome(word)) {
                 System.out.println(word);
             }
         }
+        System.out.println("Palindromes printed.");
+        */
+
+        // Off by one
+        System.out.println("Palindromes off by one: ");
+        while (!in.isEmpty()) {
+            String word = in.readString();
+            if (word.length() >= minLength && palindrome.isPalindrome(word, offByOne)) {
+                System.out.println(word);
+                //System.out.println("There's nothing left");
+
+            }
+        }
+
     }
 }
- Uncomment this class once you've written isPalindrome. */
