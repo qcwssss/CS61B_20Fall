fce2812d00f3dbb6c74ad93f177b86315e86d2d5
Chen Qiu <qiu.chen1@northeastern.edu>
2021-06-17 10:59:13 -0700

    practice ArraySet

---
+++ 6.1Lists,Sets,ArraySet/ArraySet.java
@@ -0,0 +1,52 @@
+public class ArraySet<T> {
+    private int size;
+    private T[] items;
+
+    public ArraySet() {
+        size = 0;
+        items = (T[]) new Object[100];
+    }
+
+    /* Returns true if this map contains a mapping for the specified key.
+     */
+    public boolean contains(T x) {
+        for (int i = 0; i < size; i++) {
+            if (items[i].equals(x)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /* Associates the specified value with the specified key in this map. 
+       Throws an IllegalArgumentException if the key is null. */
+    public void add(T x) {
+        if (contains(x)) {
+            return;
+        }
+        items[size] = x;
+        size++;
+    }
+
+    /* Returns the number of key-value mappings in this map. */
+    public int size() {
+        return size;
+    }
+
+    public static void main(String[] args) {
+        ArraySet<String> s = new ArraySet<>();
+        //s.add(null);
+        s.add("horse");
+        s.add("fish");
+        s.add("house");
+        s.add("fish");        
+        System.out.println(s.contains("horse"));        
+        System.out.println(s.size());       
+    }
+
+    /* Also to do:
+    1. Make ArraySet implement the Iterable<T> interface.
+    2. Implement a toString method.
+    3. Implement an equals() method.
+    */
+}
