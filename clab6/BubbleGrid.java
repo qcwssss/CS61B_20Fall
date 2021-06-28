d462bd31f9162386906c932bc956be2058844ca0
Chen Qiu <qiu.chen1@northeastern.edu>
2021-06-28 10:25:00 -0700

    update clab6 skeleton

---
+++ clab6/BubbleGrid.java
@@ -0,0 +1,90 @@
+import java.util.Arrays;
+import java.util.Objects;
+
+/**
+ * Falling Bubbles.
+ * lab6 cs61b-berkley
+ * @author Chen Qiu
+ *
+ * A grid of bubbles.
+ */
+public class BubbleGrid {
+
+	private int numOfRows;
+	private int numOfColumns;
+	private int[][] grid;
+
+
+	/**
+	 * Constructor #1.
+	 * Creates a BubbleGrid.
+	 */
+	public BubbleGrid(int[][] grid) {
+		this.grid = grid;
+		numOfRows = grid.length;
+		numOfColumns = grid[0].length;
+	}
+
+	/**
+	 * Return bubble grid.
+	 * @return this grid
+	 */
+	public int[][] getGrid() {
+		return grid;
+	}
+
+	private enum Position{
+		UP,
+		DOWN,
+		LEFT,
+		RIGHT
+	}
+
+	/**
+	 * A helper method to union stuck bubbles.
+	 * Rule: A bubble is stuck if
+	 * 	• It is in the topmost row of the grid, or
+	 * 	• It is orthogonally adjacent to a bubble that is stuck.
+	 * @param grid bubble grid
+	 */
+	private void connectBubbles(BubbleGrid grid) {
+		// connect stuck bubbles
+		int height = grid.numOfRows;
+		int width = grid.numOfColumns;
+
+		// store indices of topmost stuck bubbles
+		int[] topmost = new int[width];
+		// unionFind
+		for (int i = 0; i <height; i ++) {
+			for (int j = 0; j < width; j++) {
+				if (grid.getGrid()[i][j] == 1) {
+					topmost[j] = 1;
+				}
+			}
+		}
+
+	}
+
+	/**
+	 * A helper method to determine the status of each bubble in
+	 * the Bubble grid.
+	 * @param bubbleGrid instance
+	 * @return int[][] stuckGrid
+	 */
+	private int[][] stuckGrid(BubbleGrid bubbleGrid) {
+		int[][] gridOfBubble = bubbleGrid.getGrid();
+		int height = bubbleGrid.numOfRows;
+		int width = bubbleGrid.numOfColumns;
+		int[][] stuckGrid = new int[height][width];
+		// loop the topmost row to mark stuck bubbles
+
+
+	}
+
+	public int[] popBubbles(int[][] darts) {
+
+		return new int[0];
+	}
+
+
+}
---
+++ clab6/UnionFind.java
@@ -0,0 +1,84 @@
+import java.util.ArrayList;
+
+public class UnionFind {
+    int[] parent;
+
+    /* Creates a UnionFind data structure holding n vertices. Initially, all
+       vertices are in disjoint sets. */
+    public UnionFind(int n) {
+        parent = new int[n];
+        // set all the parents to be -1 to symbolize that they are disjoint
+        for (int i = 0; i < n; i++) {
+            parent[i] = -1;
+        }
+    }
+
+    /* Throws an exception if v1 is not a valid vertex. */
+    private void validate(int v1) {
+        // TODO
+        if (v1 > parent.length - 1) {
+            throw new IllegalArgumentException("Invalid vertex: " + v1);
+        }
+    }
+
+    /* Returns the size of the set v1 belongs to. */
+    public int sizeOf(int v1) {
+        validate(v1);
+        // root stores the value -size
+        int root = find(v1);
+        return -parent[root];
+    }
+
+    /* Returns the parent of v1. If v1 is the root of a tree, returns the
+       negative size of the tree for which v1 is the root. */
+    public int parent(int v1) {
+        validate(v1);
+        return parent[v1];
+    }
+
+    /* Returns true if nodes v1 and v2 are connected. */
+    public boolean isConnected(int v1, int v2) {
+        // TODO
+        validate(v1);
+        validate(v2);
+
+        return find(v1) == find(v2);
+    }
+
+    /* Connects two elements v1 and v2 together. v1 and v2 can be any valid 
+       elements, and a union-by-size heuristic is used.
+       If the sizes of the sets are equal, tie break by connecting v1's root to v2's root.
+       Connecting a vertex with itself or vertices that are already connected should not
+       change the sets but may alter the internal structure of the data. */
+    public void connect(int v1, int v2) {
+        // TODO
+        validate(v1);
+        validate(v2);
+        if (isConnected(v1, v2)) {
+            return;
+        }
+
+        int root1 = find(v1);
+        int root2 = find(v2);
+        int newRoot = sizeOf(v1) + sizeOf(v2);
+        if (sizeOf(v1) > sizeOf(v2)) {
+            parent[root2] = root1;
+            parent[root1] = -newRoot;
+        } else {
+            parent[root1] = root2;
+            parent[root2] = -newRoot;
+        }
+    }
+
+    /* Returns the root of the set v1 belongs to. Path-compression is employed
+       allowing for fast search-time. */
+    public int find(int v1) {
+        // TODO
+        validate(v1);
+        if (parent[v1] < 0) {
+            return v1;
+        }
+        return find(parent[v1]);
+    }
+
+}
