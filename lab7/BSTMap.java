9d8c737bfe7d4e3afc5712e21b320df436e3e683
Chen Qiu <qiu.chen1@northeastern.edu>
2021-07-05 20:59:47 -0700

    start lab7

---
+++ lab7/BSTMap.java
@@ -0,0 +1,55 @@
+import java.util.Iterator;
+import java.util.Set;
+
+public class BSTMap<K,V> implements Map61B{
+	@Override
+	public void clear() {
+
+	}
+
+	@Override
+	public boolean containsKey(Object key) {
+		return false;
+	}
+
+	@Override
+	public Object get(Object key) {
+		return null;
+	}
+
+	@Override
+	public int size() {
+		return 0;
+	}
+
+	@Override
+	public void put(Object key, Object value) {
+
+	}
+
+	@Override
+	public Set keySet() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Object remove(Object key) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Object remove(Object key, Object value) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Iterator iterator() {
+		throw new UnsupportedOperationException();
+
+	}
+
+	/** Prints out your BSTMap in order of increasing Key.*/
+	public void printInOrder() {
+
+	}
+}
