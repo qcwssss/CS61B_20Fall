dc1dd68bff24310b5f49cb3583c8422d401f1b27
Chen Qiu <qiu.chen1@northeastern.edu>
2021-07-08 16:51:40 -0700

    complete experiment1()

--- clab7/Experiments.java
+++ clab7/Experiments.java
@@ -1,8 +1,41 @@
+import org.knowm.xchart.SwingWrapper;
+import org.knowm.xchart.XYChart;
+import org.knowm.xchart.XYChartBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
  * Created by hug.
  */
 public class Experiments {
     public static void experiment1() {
+        int numberOfItems =  5000;
+        Random r = new Random();
+        BST randTree = new BST();
+        // chart info
+        List<Integer> xVal = new ArrayList<>();
+        List<Double> yAvgDepth = new ArrayList<>();
+        List<Double> yOptAvgDepth = new ArrayList<>();
+
+        for (int x = 0; x < numberOfItems; x++) {
+            int addItems = r.nextInt(100);
+            // if contains, skip
+            if (!randTree.contains(addItems)){
+                randTree.add(addItems); // add items to BST
+            }
+
+            xVal.add(x);
+            yAvgDepth.add(randTree.averageDepth());
+            yOptAvgDepth.add(ExperimentHelper.optimalAverageDepth(x));
+        }
+
+        XYChart chart = new XYChartBuilder().width(800).height(600).xAxisTitle("x items").yAxisTitle("y average_depth").build();
+        chart.addSeries("average depth", xVal, yAvgDepth);
+        chart.addSeries("optimal average depth", xVal, yOptAvgDepth);
+
+        new SwingWrapper(chart).displayChart();
     }
 
     public static void experiment2() {
@@ -12,5 +45,6 @@ public class Experiments {
     }
 
     public static void main(String[] args) {
+        experiment1();
     }
 }
