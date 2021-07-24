efc1cbb7c7c85c1f30ef98a7cc8ba07c0dff58e6
Chen Qiu <qiu.chen1@northeastern.edu>
2021-07-24 16:33:15 -0700

    copy from others solution

--- clab8/FlightSolver.java
+++ clab8/FlightSolver.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 /**
  * Solver for the Flight problem (#9) from CS 61B Spring 2018 Midterm 2.
@@ -7,14 +9,40 @@ import java.util.ArrayList;
  * considered to be in the air at the same time.
  */
 public class FlightSolver {
+    //private PriorityQueue<Flight> pq;
+    private int maxPassengers;
 
     public FlightSolver(ArrayList<Flight> flights) {
-        /* FIX ME */
+        Comparator<Flight> startTimeCmp = (Flight o1, Flight o2) -> (o1.startTime - o2.startTime);
+        Comparator<Flight> endTimeCmp = (Flight o1, Flight o2) -> (o1.endTime - o2.endTime);
+        // public PriorityQueue(Comparator<? super E> comparator);
+        PriorityQueue<Flight> startMinPQ = new PriorityQueue<>(startTimeCmp);
+        PriorityQueue<Flight> endMinPQ = new PriorityQueue<>(endTimeCmp);
+
+        startMinPQ.addAll(flights);
+        endMinPQ.addAll(flights);
+        // print pq
+        System.out.println("startMinPQ: "+ startMinPQ);
+        for (Flight f : startMinPQ) {
+            System.out.print( f.startTime + " ");
+
+        }
+        System.out.println("endMinPQ: "+ endMinPQ.peek().endTime);
+
+        int nowPassengers = 0;  //To store current timestamp passenger number
+        while (startMinPQ.peek() != null) {
+            //compare the smallest startTime with the smallest endTime
+            if (startMinPQ.peek().startTime <= endMinPQ.peek().endTime) {
+                nowPassengers += startMinPQ.poll().passengers;
+                if (nowPassengers > maxPassengers) maxPassengers = nowPassengers;
+            } else {
+                nowPassengers -= endMinPQ.poll().passengers;
+            }
+        }
     }
 
     public int solve() {
-        /* FIX ME */
-        return -1;
+        return maxPassengers;
     }
 
 }
