c33478da901ab3558fefde262a0680d23385722c
Chen Qiu <qiu.chen1@northeastern.edu>
2021-07-29 11:04:32 -0700

    update sweep line solution

--- clab8/FlightSolver.java
+++ clab8/FlightSolver.java
@@ -1,48 +1,36 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
 /**
- * Solver for the Flight problem (#9) from CS 61B Spring 2018 Midterm 2.
- * Assumes valid input, i.e. all Flight start times are >= end times.
- * If a flight starts at the same time as a flight's end time, they are
- * considered to be in the air at the same time.
+ * Solver for the Flight problem (#9) from CS 61B Spring 2018 Midterm 2. Assumes valid input, i.e.
+ * all Flight start times are >= end times. If a flight starts at the same time as a flight's end
+ * time, they are considered to be in the air at the same time.
  */
 public class FlightSolver {
-    //private PriorityQueue<Flight> pq;
-    private int maxPassengers;
+  // private PriorityQueue<Flight> pq;
+  private int maxPassengers;
 
-    public FlightSolver(ArrayList<Flight> flights) {
-        Comparator<Flight> startTimeCmp = (Flight o1, Flight o2) -> (o1.startTime - o2.startTime);
-        Comparator<Flight> endTimeCmp = (Flight o1, Flight o2) -> (o1.endTime - o2.endTime);
-        // public PriorityQueue(Comparator<? super E> comparator);
-        PriorityQueue<Flight> startMinPQ = new PriorityQueue<>(startTimeCmp);
-        PriorityQueue<Flight> endMinPQ = new PriorityQueue<>(endTimeCmp);
-
-        startMinPQ.addAll(flights);
-        endMinPQ.addAll(flights);
-        // print pq
-        System.out.println("startMinPQ: "+ startMinPQ);
-        for (Flight f : startMinPQ) {
-            System.out.print( f.startTime + " ");
-
-        }
-        System.out.println("endMinPQ: "+ endMinPQ.peek().endTime);
-
-        int nowPassengers = 0;  //To store current timestamp passenger number
-        while (startMinPQ.peek() != null) {
-            //compare the smallest startTime with the smallest endTime
-            if (startMinPQ.peek().startTime <= endMinPQ.peek().endTime) {
-                nowPassengers += startMinPQ.poll().passengers;
-                if (nowPassengers > maxPassengers) maxPassengers = nowPassengers;
-            } else {
-                nowPassengers -= endMinPQ.poll().passengers;
-            }
-        }
+  public FlightSolver(ArrayList<Flight> flights) {
+    // sweep line
+    ArrayList<int[]> weights = new ArrayList<>(flights.size() * 2);
+    for (Flight f : flights) {
+      weights.add(new int[] {f.startTime, f.passengers});
+      weights.add(new int[] {f.endTime, -f.passengers});
     }
-
-    public int solve() {
-        return maxPassengers;
+    Collections.sort(weights, (a, b) -> (a[0] - b[0])); // ascending order
+    // scan
+    int count = 0;
+    int res = 0; // store max passengers on air
+    for (int[] i : weights) {
+      count += i[1];
+      res = Math.max(res, count);
     }
+    maxPassengers = res;
+  }
 
+  public int solve() {
+    return maxPassengers;
+  }
 }
