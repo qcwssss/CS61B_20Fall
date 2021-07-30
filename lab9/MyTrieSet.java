ed77fc1df603e6621ef44ba55442b4967b06d85d
Chen Qiu <qiu.chen1@northeastern.edu>
2021-07-30 10:01:37 -0700

    update lab9 skeleton

---
+++ lab9/MyTrieSet.java
@@ -0,0 +1,53 @@
+import java.util.List;
+
+public class MyTrieSet implements TrieSet61B{
+	/**
+	 * Clears all items out of Trie
+	 */
+	@Override
+	public void clear() {
+
+	}
+
+	/**
+	 * Returns true if the Trie contains KEY, false otherwise
+	 *
+	 * @param key
+	 */
+	@Override
+	public boolean contains(String key) {
+		return false;
+	}
+
+	/**
+	 * Inserts string KEY into Trie
+	 *
+	 * @param key
+	 */
+	@Override
+	public void add(String key) {
+
+	}
+
+	/**
+	 * Returns a list of all words that start with PREFIX
+	 *
+	 * @param prefix
+	 */
+	@Override
+	public List<String> keysWithPrefix(String prefix) {
+		return null;
+	}
+
+	/**
+	 * Returns the longest prefix of KEY that exists in the Trie
+	 * Not required for Lab 9. If you don't implement this, throw an
+	 * UnsupportedOperationException.
+	 *
+	 * @param key
+	 */
+	@Override
+	public String longestPrefixOf(String key) {
+		return null;
+	}
+}
