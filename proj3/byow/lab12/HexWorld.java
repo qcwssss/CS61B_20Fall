0dd78a1b45910850cf2158c39cce071104ff6497
Chen Qiu <qiu.chen1@northeastern.edu>
2021-09-02 16:51:01 -0700

    complete lab12, add tesselation succesfully

--- proj3/byow/lab12/HexWorld.java
+++ proj3/byow/lab12/HexWorld.java
@@ -1,22 +1,18 @@
 package byow.lab12;
-import org.junit.Test;
-import static org.junit.Assert.*;
 
 import byow.TileEngine.TERenderer;
 import byow.TileEngine.TETile;
 import byow.TileEngine.Tileset;
 
-import java.awt.*;
 import java.util.Random;
 
 /**
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
-	private static final int WIDTH = 30;
-	private static final int HEIGHT = 30;
+	private static final int WIDTH = 50;
+	private static final int HEIGHT = 50;
 
-	//private TETile[][] hexWorld;
 	private static final long SEED = 2873123;
 	private static final Random RANDOM = new Random(SEED);
 
@@ -32,14 +28,19 @@ public class HexWorld {
 		return world;
 	}
 
-
+	/**
+	 * Add a hexagon filled with random tiles to the world board.
+	 * @param size size of hexagon
+	 * @param xPos x location
+	 * @param yPos y location
+	 * @param board a TETile grid
+	 */
 	public void addHexagon(int size, int xPos, int yPos, TETile[][] board) {
 		if (size < 2) {
 			throw new IllegalArgumentException("size must be a positive integer larger than 1");
 		}
-		// need to check x, y position first
-		checkLocation(xPos, yPos, size);
 
+		checkLocation(xPos, yPos, size); // need to check x, y position first
 		TETile randomTile = randomTile();
 		// upper half
 		addUpperHalf(size, xPos, yPos, board, randomTile);
@@ -89,14 +90,33 @@ public class HexWorld {
 		}
 	}
 
+	private void addColumnsOfHex(int size, int x, int y, int num, TETile[][] world) {
+		for (int i = 0 ; i < num; i++) {
+			addHexagon(size, x, y - size *2*i, world);
+			// from top to bottom
+		}
+	}
+
+	public void addTesselation(int size, int x, int y, TETile[][] board) {
+		int tri = 3;
+		for(int i = 0; i < 5; i++) {
+			if (i < 3) {
+				addColumnsOfHex(size, x + (size * 2 - 1) * i, y + size * i, tri + i, board);
+			} else {
+				addColumnsOfHex(size, x + (size * 2 - 1) * i, y + 2*size - size * (i - tri + 1) , 4 - i + tri, board);
+			}
+		}
+	}
+
 	public static void main(String[] args) {
 		HexWorld hex = new HexWorld();
 		TERenderer ter = new TERenderer();
 		ter.initialize(WIDTH, HEIGHT);
 
 		TETile[][] hexBoard = buildBoardWithNothing();
-		// size 3 hexagon locates at bottom left 
-		hex.addHexagon(3, 2, 5, hexBoard);
+		// size can be 2, 3, 4;
+		int size = 4;
+		hex.addTesselation(size, 4, HEIGHT - 3 * size  , hexBoard);
 
 		ter.renderFrame(hexBoard);
 	}
