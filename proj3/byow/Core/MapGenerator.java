e01a84f8bab61d603bf2f9fade7783a8a3750e29
Chen Qiu <qiu.chen1@northeastern.edu>
2021-09-04 09:59:48 -0700

    create MapGenerator class

---
+++ proj3/byow/Core/MapGenerator.java
@@ -0,0 +1,19 @@
+package byow.Core;
+
+/**
+ * Create by Chen.
+ * Generate a map of tiles.
+ *
+ */
+public class MapGenerator {
+	/*
+	1. Create a tile grid
+	buildEmptyMap(int width, int height)
+	2. Helper methods to create all kinds of rooms, hall ways, etc.
+	buildWall(int length, int xStart, int yStart, int direct)
+	buildHallWay
+	buildRoom
+	connectRoom
+
+	 */
+}
