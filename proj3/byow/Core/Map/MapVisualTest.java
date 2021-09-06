98330b7a3409b1beb0310bf29be098667d1f3fc7
Chen Qiu <qiu.chen1@northeastern.edu>
2021-09-06 15:50:45 -0700

    update neighbors in visual test

--- proj3/byow/Core/Map/MapVisualTest.java
+++ proj3/byow/Core/Map/MapVisualTest.java
@@ -1,24 +1,47 @@
 package byow.Core.Map;
 
+import byow.Core.Map.World.WorldGraph;
 import byow.TileEngine.TERenderer;
 import byow.TileEngine.TETile;
+import org.junit.Test;
 
+import java.util.List;
 import java.util.Random;
 
+import static byow.Core.Engine.WIDTH;
 import static byow.Core.Map.MapGenerator.buildEmptyMap;
 
 public class MapVisualTest {
-	static final Random random = new Random(23217);
+	private static final Random random = new Random(23217);
+	private final int WIDTH = 80, HEIGHT = 40;
 
-	public static void main(String[] args) {
-		final int WIDTH = 80, HEIGHT = 40;
+
+	@Test
+	public void testWGraphConstructor() {
+		List<Room> roomList = buildRoomList();
+		WorldGraph wg = new WorldGraph(roomList);
+		wg.toString();
+
+	}
+	private List<Room> buildRoomList() {
+		TETile[][] grid = buildEmptyMap(WIDTH ,HEIGHT);
+		MapGenerator map = new MapGenerator(grid);
+		return map.createRandomRooms(random);
+
+	}
+
+
+	public void main(String[] args) {
+		//final int WIDTH = 80, HEIGHT = 40;
 		TETile[][] grid = buildEmptyMap(WIDTH ,HEIGHT);
 
 		TERenderer ter = new TERenderer();
 		ter.initialize(WIDTH, HEIGHT);
 
 		MapGenerator map = new MapGenerator(grid);
-		map.createRandomRooms(random);
+		List<Room> roomList = map.createRandomRooms(random);
+
+		WorldGraph wg = new WorldGraph(roomList);
 
 		ter.renderFrame(grid);
 
---
+++ proj3/byow/Core/Map/World/TestWorldGraph.java
@@ -0,0 +1,21 @@
+package byow.Core.Map.World;
+
+
+import byow.Core.Map.World.WorldGraph;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestWorldGraph {
+	WorldGraph wg;
+
+
+	@Before
+	public void setUp() {
+
+	}
+
+	@Test
+	public void testWGraphConstructor() {
+		//wg = new WorldGraph();
+	}
+}
