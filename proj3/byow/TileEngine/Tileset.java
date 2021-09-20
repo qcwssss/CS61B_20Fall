4938d4f610197bda529fba768fef179c53ae8dc1
Chen Qiu <qiu.chen1@northeastern.edu>
2021-09-19 20:46:44 -0700

    update key tile

--- proj3/byow/TileEngine/Tileset.java
+++ proj3/byow/TileEngine/Tileset.java
@@ -33,6 +33,9 @@ public class Tileset {
     public static final TETile SAND = new TETile('▒', Color.yellow, Color.black, "sand");
     public static final TETile MOUNTAIN = new TETile('▲', Color.gray, Color.black, "mountain");
     public static final TETile TREE = new TETile('♠', Color.green, Color.black, "tree");
+
+    public static final TETile KEY = new TETile('¶', Color.ORANGE, Color.black, "key");
+
 }
 
 
