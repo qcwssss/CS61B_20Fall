d3f2dedd5b3d8abd5fe550018986d64a72874e66
Chen Qiu <qiu.chen1@northeastern.edu>
2021-09-20 10:16:57 -0700

    restore?

--- reference_solution/cs61b-1-master/proj1b/OffByOne.java
+++
@@ -1,7 +0,0 @@
-public class OffByOne implements CharacterComparator {
-    @Override
-    public boolean equalChars(char x, char y) {
-        int diff = x - y;
-        return diff == 1 || diff == -1;
-    }
-}
